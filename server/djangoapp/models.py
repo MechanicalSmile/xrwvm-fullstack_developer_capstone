36d0b4141c71e40463b0c55a44c4ae3c4e21f70a
MechanicalSmile <afour20moment@gmail.com>
2024-08-13 03:58:10 -0400

    created CarModel and CarMake Models, Views, Url and Populate Data

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,25 +1,55 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('COMPACT', 'Compact'),
+        ('SEDAN', 'Sedan'),
+        ('WAGON', 'Wagon'),
+        ('SUV', 'SUV'),
+        ('TRUCK', 'Truck'),
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='COMPACT')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ])
+
+    COLORS = [
+        ('BLACK', 'Black'),
+        ('WHITE', 'White'),
+        ('BLUE', 'Blue'),
+        ('RED', 'Red'),
+        ('GREEN', ' Green'),
+        ('YELLOW', 'Yellow'),
+    ]
+    color = models.CharField(max_length=10, choices=COLORS, default='BLACK')
+
+    SHADES = [
+        ('', '------'),
+        ('LIGHT', 'Light'),
+        ('DARK', 'Dark')
+    ]
+
+    shade = models.CharField(max_length=10, choices=SHADES, default='', blank=True)
+
+    def __str__(self):
+        return self.name
 # - __str__ method to print a car make object
