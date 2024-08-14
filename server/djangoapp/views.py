a7af9eb28af1a4b337d8adda640dac8fa75ae163
MechanicalSmile <afour20moment@gmail.com>
2024-08-14 05:47:14 -0400

    added dynaic pages for displaying dealers, reviews, review submission

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -30,8 +30,9 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake":car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
+    
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
