fd7b42a5b440e63e99d39ea4900421ef87a3c947
MechanicalSmile <107344821+MechanicalSmile@users.noreply.github.com>
2024-08-15 09:41:04 +0300

    Update populate.py
    edited for linting

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -80,7 +80,7 @@ def initiate():
        "type": "SUV",
        "year": 2023,
        "car_make": car_make_instances[4]},
-        # Add more CarModel instances as needed
+    # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
@@ -90,4 +90,3 @@ def initiate():
             type=data['type'],
             year=data['year']
         )
-        
